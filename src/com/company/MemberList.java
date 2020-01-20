7c5710791e5fb61ea380e071f67d5e879ac841ca
Emmanuel Ndziba <Emmanuel_Ndziba@hotmaill.se>
2020-01-20 12:11:59 +0100

    jobbar på det olika classerna och försöker få ett sammanhang

---
+++ src/com/company/MemberList.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MemberList {
+
+    // Lista på Användare i biblioteket
+    private List<Person> members  = new ArrayList<>();
+    private Scanner input = new Scanner(System.in);
+}
